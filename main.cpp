c4939d9448371923feb201b18b7e72528dcba5ce
Karina Romero <betzy_skamm@hotmail.com>
2019-07-17 11:20:29 -0700

    moving character and buil map

--- main.cpp
+++ main.cpp
@@ -1,30 +1,75 @@
 #include <iostream>
 #include <cmath>
+#include <array>
 
 using namespace std;
 
-void DrawMap(int heroPosition, char gameMap[5])
+void DrawMap(int heroPositionR, int heroPositionC, char gameMap[5][5])
 {
-    for (int i = 0; i < 5; i++)
+    for (int r = 0; r < 5; r++)
     {
-        if(i != heroPosition)
+        for (int c = 0; c < 5; c++)
         {
-            cout << gameMap[i];
-
+            if (r != heroPositionC)
+            {
+                cout << gameMap[c][r] << " ";
+            }
+            else
+            {
+                if (c != heroPositionR)
+                {
+                    cout << gameMap[c][r] << " ";
+                }
+                else
+                {
+                    cout << 'H'<< " ";
+                }
+            }
         }
-        else
-        {
-            cout << 'H';
-        }
-        
+        cout << endl;
     }
 };
 
 int main()
 {
-    int heroPosition = 2;
-    char map[5] = {'1','1','1','1','1'};
-    DrawMap(heroPosition, map);
-    
-    
+    int heroPositionR = 0;
+    int heroPositionC = 0;
+    bool isGameOver = false;
+    char input = ' ';
+    char map[5][5] = {
+        {'1', '1', '1', '1', '1'},
+        {'1', '1', '1', '1', '1'},
+        {'1', '1', '1', '1', '1'},
+        {'1', '1', '1', '1', '1'},
+        {'1', '1', '1', '1', '1'}};
+
+    DrawMap(heroPositionR, heroPositionC, map);
+
+    while (!isGameOver)
+    {
+        cin >> input;
+
+        switch (input)
+        {
+        case 'd':
+            heroPositionR++;
+            break;
+        case 'a':
+            heroPositionR--;
+            break;
+        case 's':
+            heroPositionC++;
+            break;
+        case 'w':
+            heroPositionC--;
+            break;
+        case 'p':
+            isGameOver = true;
+            break;
+
+        default:
+            break;
+        }
+        DrawMap(heroPositionR, heroPositionC, map);
+    }
 }
